aed5c36c7754cb2ebff7f1747a49f042039f0908
Kyra Henningson <kyrahenningson@hotmail.com>
2022-10-30 22:59:16 +0000

    test query

--- server/db/queries/getContactsByPatientID.js
+++ server/db/queries/getContactsByPatientID.js
@@ -2,7 +2,7 @@ const db = require("../../configs/db.config");
 
 const getContactsByPatientID = (patientID) => {
   const values = [patientID];
-  const queryString = `SELECT * FROM hps WHERE hps.patient_id = $1;`;
+  const queryString = `SELECT * FROM hps JOIN patients_health_professionals ON hps.id = patients_health_professionals.hp_id WHERE patient_id = $1`;
   return db.query(queryString, values)
   .then(data => {
     console.log(`data.rows: `, data.rows);
