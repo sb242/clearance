d1435a370fa508431a59fc5a0f2dde2a60e1ef90
Spencer <spencerb242@proton.me>
2022-10-31 01:54:52 -0600

    refactored MedsList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react";
-
 import "./App.css";
 import Nav from "./components/nav";
 import About from "./components/about";
@@ -7,23 +5,11 @@ import Login from "./components/login";
 import MedsList from "./components/MedsList";
 import UserDisplay from "./components/UserDisplay";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import axios from "axios";
 
 // use effect with axios calls to set state with data from api call
 // then pass down data to MedsList as props
 
 function App() {
-  const [medicine, setMedicine] = useState([]);
-
-  useEffect(() => {
-    const fetchMedicine = async () => {
-      const response = await axios("http://localhost:8080/medications");
-      const data = await response;
-      setMedicine(data.data);
-    };
-    fetchMedicine();
-  }, []);
-
   return (
     <Router>
       <div className="App">
@@ -31,10 +17,7 @@ function App() {
           <Route path="/" exact component={UserDisplay} />
           <Route path="/about" component={About} />
           <Route path="/login" component={Login} />
-          <Route
-            path="/medications"
-            render={(props) => <MedsList {...props} medicine={medicine} />}
-          />
+          <Route path="/medications" component={MedsList} />
         </Switch>
       </div>
     </Router>
--- client/src/components/MedsList.js
+++ client/src/components/MedsList.js
@@ -1,27 +1,36 @@
 import MedsListItem from "./MedsListItem";
-import 'antd/dist/antd.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "antd/dist/antd.css";
 
 // pass current data and prior data received from App.js as props to MedsListItem
 
-export default function MedsList(props) {
+export default function MedsList() {
+  const [medicine, setMedicine] = useState([]);
 
-  const currentData = props.medicine.filter((item) => {
-    return item.end_date === null
-  })
+  useEffect(() => {
+    const fetchMedicine = async () => {
+      const response = await axios("http://localhost:8080/medications");
+      const data = await response;
+      setMedicine(data.data);
+    };
+    fetchMedicine();
+  }, []);
 
-  const priorData = props.medicine.filter((item) => {
-    return item.end_date !== null
-  })
+  const currentData = medicine.filter((item) => {
+    return item.end_date === null;
+  });
+
+  const priorData = medicine.filter((item) => {
+    return item.end_date !== null;
+  });
 
   return (
     <div className="meds-page">
       <h2>Medications</h2>
       <br></br>
       <h3>Existing</h3>
-      <MedsListItem
-        currentData={currentData}
-        priorData={priorData}
-      />
+      <MedsListItem currentData={currentData} priorData={priorData} />
     </div>
   );
 }
