97d96862f20e2b17ada8bd35e9f6b836775c7b07
Kyra Henningson <kyrahenningson@hotmail.com>
2022-11-02 10:27:50 -0700

    client side logic to delete contact, and modifications to table action

--- client/src/components/AllergyTable.js
+++ client/src/components/AllergyTable.js
@@ -1,44 +1,71 @@
-import { Table, Button } from "antd";
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
-
-const columns = [
-  {
-    title: "Type",
-    dataIndex: "type",
-  },
-  {
-    title: "Anaphylactic",
-    dataIndex: "anaphylactic",
-    render: (text) => String(text),
-  },
-  {
-    title: "Sensitivity",
-    dataIndex: "sensitivity",
-    render: (text) => String(text),
-  },
-  {
-    title: "Intolerance",
-    dataIndex: "intolerance",
-    render: (text) => String(text),
-  },
-  {
-    title: "Actions",
-    render: (_, record) => {
-      return (
-        <>
-          <Button type="link">
-            <EditOutlined />
-          </Button>
-          <Button type="link">
-            <DeleteOutlined />
-          </Button>
-        </>
-      );
-    },
-  },
-];
+import { Table, Button, Popconfirm } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import axios from "axios";
 
 function AllergyTable(props) {
+  const deleteAllergy = function (patientID) {
+    return axios.delete(`/allergy/${patientID}`).then((res) => {
+      return props.fetchContacts();
+    });
+  };
+
+  const handleDelete = (record) => {
+    deleteAllergy(record.patient_id);
+  };
+
+
+  const columns = [
+    {
+      title: "Type",
+      dataIndex: "type",
+    },
+    {
+      title: "Anaphylactic",
+      dataIndex: "anaphylactic",
+      render: (text) => String(text),
+    },
+    {
+      title: "Sensitivity",
+      dataIndex: "sensitivity",
+      render: (text) => String(text),
+    },
+    {
+      title: "Intolerance",
+      dataIndex: "intolerance",
+      render: (text) => String(text),
+    },
+    //KEEP HERE FOR REFERENCE TO WAY USED PRIOR TO DELETE
+    // {
+    //   title: "Actions",
+    //   render: (_, record) => {
+    //     return (
+    //       <>
+    //         <Button type="link">
+    //           <EditOutlined />
+    //         </Button>
+    //         <Button type="link">
+    //           <DeleteOutlined />
+    //         </Button>
+    //       </>
+    //     );
+    //   },
+    // },
+    {
+      title: "Actions",
+      dataIndex: "operation",
+      render: (_, record) =>
+        props.allergies.length >= 1 ? (
+          <Popconfirm
+            title="Are you sure?"
+            onConfirm={() => handleDelete(record)}
+          >
+            <Button type="link">
+              <DeleteOutlined />
+            </Button>
+          </Popconfirm>
+        ) : null,
+    },
+  ];
   return (
     <div>
       <Table
