550cfca1a097ebba7d7edc7d56be694d4b438493
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-11-02 22:06:48 -0700

    able to edit and save contacts

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -33,7 +33,7 @@ function Contacts() {
     console.log("Failed to submit form:", errorInfo);
   };
 
-  const fetchContacts = function () {
+  const fetchContacts = function() {
     return axios.get("/contacts?patientID=1").then((res) => {
       return setContacts(res.data.contacts);
     });
@@ -43,7 +43,7 @@ function Contacts() {
     fetchContacts();
   }, []);
 
-  const addContact = function (values) {
+  const addContact = function(values) {
     return axios.post("/contacts", values).then((res) => {
       return fetchContacts();
     });
@@ -134,7 +134,7 @@ function Contacts() {
             ></Form.Item>
           </Form>
         </Modal>
-        <ContactsTable fetchContacts={fetchContacts} contacts={contacts} />
+        <ContactsTable fetchContacts={fetchContacts} contacts={contacts} setContacts={setContacts} />
       </div>
     </Layout>
   );
