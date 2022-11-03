fb28b5ff699f08f7a4392cc711f394ab6bc4b6d1
Spencer <spencerb242@proton.me>
2022-11-03 11:54:49 -0600

    updated .env.example

--- server/routes/sendEmail.js
+++ server/routes/sendEmail.js
@@ -12,9 +12,7 @@ router.post("/send", (req, res) => {
   const body = `Hello ${contactName},<br><br>Patient: ${patientFirstName} ${patientLastName} has sent you their updated records.<br><br>Please find this attached as a pdf.<br><br>Thank you,<br><br>Clearance`;
 
   contactsQueries.getEmailByContactName(contactName).then((data) => {
-    console.log("data", data);
     const email = data[0].email;
-    console.log("email", email);
     nodeMailer(email, subject, body).catch(console.error);
   });
   res.sendStatus(200);
