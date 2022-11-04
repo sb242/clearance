368f8afa3acb26f21da9018a8bb75fb20d674c9d
Kyra Henningson <kyrahenningson@hotmail.com>
2022-11-04 16:39:30 -0700

    change landing page layout and images, create logo

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,19 +1,12 @@
 import "./index.css";
-import landingPageImage from "../../assets/landingpage.svg";
-import Nav from "./Nav";
+import logo from "../../assets/Clearance-logo2.png";
 import Login from "./Login";
-import { OmitProps } from "antd/lib/transfer/ListBody";
 
 export default function LandingPage(props) {
   return (
     <div className="container">
-      <Nav />
-      <div className="content">
-        <div className="landing-image">
-          <img src={landingPageImage} alt="" />
-        </div>
-        <Login className="login" onClick={props.onClick} />
-      </div>
+      <img className="logo-image" src={logo} />
+      <Login className="login" onClick={props.onClick} />
     </div>
   );
 }
