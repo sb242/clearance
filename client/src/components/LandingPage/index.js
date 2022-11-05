9cca15547f73dec8ff44f83dfebdaa784edd654f
Kyra Henningson <kyrahenningson@hotmail.com>
2022-11-05 11:57:56 -0700

    move heart in relation to svg

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,12 +1,14 @@
 import "./index.css";
 import logo from "../../assets/Clearance-logo-bold.png";
 import Login from "./Login";
-import Heart from "../Heart.js"
+import Heart from "../Heart.js";
 
 export default function LandingPage(props) {
   return (
     <div>
-      <div className="container"><Heart/></div>
+      <div className="container">
+        <Heart />
+      </div>
       <img className="logo-image" src={logo} />
       <Login className="login" onClick={props.onClick} />
     </div>
