6ebd198b31bf87ddbd210cdfba33d0406708a670
Kyra Henningson <kyrahenningson@hotmail.com>
2022-11-04 23:26:47 -0700

    render png over svg file using transition instead of z-index

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,10 +1,12 @@
 import "./index.css";
 import logo from "../../assets/Clearance-logo-bold.png";
 import Login from "./Login";
+import backgroundImage from "../../assets/undraw_medicine.svg"
 
 export default function LandingPage(props) {
   return (
-    <div className="container">
+    <div >
+      <img className="container" src={backgroundImage}/>
       <img className="logo-image" src={logo} />
       <Login className="login" onClick={props.onClick} />
     </div>
